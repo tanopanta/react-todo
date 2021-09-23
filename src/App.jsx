ddabb10bee1e14649f32c18607a3b9097bfc149e
tanopanta <25007602+tanopanta@users.noreply.github.com>
2021-09-23 15:12:03 +0900

    add limit

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTODOは５個までです</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
